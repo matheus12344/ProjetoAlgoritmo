b29e3a6e03f56ced419b07575849b7e8fb2ac7d6
André Fiker <andrefiker@gmail.com>
2026-05-07 16:35:05 -0300

    Add redirect for terapia Guarulhos path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/terapia/guarulhos",
+        destination: "/terapia-guarulhos",
+        permanent: true,
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
